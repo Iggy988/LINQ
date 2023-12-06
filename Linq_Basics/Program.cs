ca3da99527da7f1f23e2c1b189876630435ce85f
Iggy988 <igor.rodic1988@gmail.com>
2023-12-06 21:33:27 +0100

    Add project files.

---
+++ Linq_Basics/Program.cs
@@ -0,0 +1,43 @@
+﻿
+var wordsNoUpperCase = new[]
+{
+    "quick", "run", "walk"
+};
+
+var wordsUpperCase = new[]
+{
+    "quick", "RUN", "walk"
+};
+
+Console.WriteLine(IsAnyWordUperCase(wordsNoUpperCase));
+Console.WriteLine(IsAnyWordUperCase(wordsUpperCase));
+
+var orderedWords = wordsNoUpperCase.OrderBy(word => word);   
+
+Console.ReadKey();
+
+//Linq method
+static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
+{
+    return words.Any(word => word.All(letter => char.IsUpper(letter)));
+}
+
+static bool IsAnyWordUperCase(IEnumerable<string> words)
+{
+    foreach (var word in words)
+    {
+        bool areAllUpperCase = true;
+        foreach (var letter in word)
+        {
+            if (char.IsLower(letter))
+            {
+                areAllUpperCase = false;
+            }
+        }
+        if (areAllUpperCase)
+        {
+            return true;
+        }
+    }
+    return false;
+}
