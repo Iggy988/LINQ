f972e9672339bf12f97681cf6c4c3160f779ec94
Iggy988 <igor.rodic1988@gmail.com>
2023-12-07 21:28:59 +0100

    Method Syntax and Query Syntax

---
+++ Method_Sintax_vs_Query_Sintax/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+var numbers = new [] { 5, 3, 7, 1, 2, 4, 12, 10, 4, 7, 5 };
+
+var smallOrderedNumbersMethodSyntax = numbers
+    .Where(num => num < 10)
+    .OrderBy(num => num)
+    .Distinct();
+
+var smallOrderedNumbersQuerySyntax = 
+    (from num in numbers
+    where num < 10
+    orderby num
+    select num).Distinct();
+
+Console.WriteLine(string.Join(", ", smallOrderedNumbersMethodSyntax) );
+Console.WriteLine(string.Join(", ", smallOrderedNumbersQuerySyntax) );
+
+Console.ReadKey();
