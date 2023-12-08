dd505310ce041ceb068895fa4cb6d4ffcf803740
Iggy988 <igor.rodic1988@gmail.com>
2023-12-08 21:52:04 +0100

    Any

---
+++ Linq_Types/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+var numbers = new[] { 5, 9, 2, 16, 6, 10, 7 };
+
+var pets = new[]
+{
+     new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+     new Pet(2, "Antony", PetType.Cat, 2f),
+     new Pet(3, "Ed", PetType.Cat, 0.7f),
+     new Pet(4, "Taiga", PetType.Dog, 35f),
+     new Pet(5, "Rex", PetType.Dog, 40f),
+     new Pet(6, "Lucky", PetType.Dog, 5f),
+     new Pet(7, "Storm", PetType.Cat, 0.9f),
+     new Pet(8, "Nyan", PetType.Cat, 2.2f),
+};
+
+
+
+//Any
+Console.WriteLine("ANY");
+bool isAnyLargerThan10 = numbers.Any(num =>num > 10);
+Console.WriteLine(isAnyLargerThan10);
+var isAnyPetNamedBruce = pets.Any(p => p.Name == "Bruce");
+Printer.Print(isAnyPetNamedBruce, nameof(isAnyPetNamedBruce));
+var isAnyFish = pets.Any(p => p.PetType == PetType.Fish);
+Printer.Print(isAnyFish, nameof(isAnyFish));
+var isThereAVerySpecificPet = pets.Any(p => p.Name.Length > 6 && p.Id % 2== 0);
+Printer.Print(isThereAVerySpecificPet, nameof(isThereAVerySpecificPet));
+var isNotEmpty = pets.Any();
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ReadKey();
---
+++ Linq_Types/Utilities/Pet.cs
@@ -0,0 +1,16 @@
+﻿
+public class Pet
+{
+    public int Id;
+    public string Name;
+    public PetType PetType;
+    public float Weight;
+
+    public Pet(int id, string name, PetType petType, float weight)
+    {
+        Id = id;
+        Name = name;
+        PetType = petType;
+        Weight = weight;
+    }
+}
