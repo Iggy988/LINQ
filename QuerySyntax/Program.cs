d8b17beedcb2b400c42660d937770ff24b2c608d
Iggy988 <igor.rodic1988@gmail.com>
2023-12-24 18:56:52 +0100

    Query syntax introduction

---
+++ QuerySyntax/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var numbers = new[] { 9, 3, 7, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+var orderedNumbers = from number in numbers
+                     let floorOfSquere = Math.Floor(Math.Sqrt(number))
+                     orderby floorOfSquere
+                     select floorOfSquere;
+Printer.Print(orderedNumbers, nameof(orderedNumbers));
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
